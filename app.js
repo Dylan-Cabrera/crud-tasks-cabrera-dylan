6e8695d6ee5110aab3d4a57d756caf0f48f3fc9d
DylanC <cabreradylan055@gmail.com>
2025-08-07 09:22:23 -0300

    Modelo task definido

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ import { StartDB } from "./src/config/db.js";
 StartDB();
 const app = express();
 dotenv.config();
-app.use(express.json());
 const PORT = process.env.PORT;
+app.use(express.json());
 
 app.listen(PORT || 3000, () => {
-    console.log(`escuchando servidor en el puerto ${PORT}`)
+    console.log(`escuchando servidor en el puerto ${PORT}`);
 });
---
+++ src/models/task.model.js
@@ -0,0 +1,25 @@
+import { sequelize } from "../config/database.js";
+import { DataTypes } from "sequelize";
+
+export const Task = sequelize.define(
+    "Task", {
+        "id": {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        "title": {
+            type: DataTypes.STRING(100),
+            unique: true,
+            allowNull: false
+        },
+        "description": {
+            type: DataTypes.STRING(100),
+            allowNull:false
+        },
+        "isComplete": {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        } 
+    }
+)
