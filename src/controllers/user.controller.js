732544be674b53b1be5366cf5dc723b5ae627a3d
DylanC <cabreradylan055@gmail.com>
2025-08-07 10:04:59 -0300

    controladores de usuariocreados

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,62 @@
+import { User } from "../models/user.model.js";
+import { Sequelize, where } from "sequelize";
+
+//Obtener todos los usuarios
+export const getAllUsers = async (req, res) => {
+    try {
+        const getAllUsers = await User.findAll();
+        res.status(200).json(getAllUsers);
+    } catch (error) {
+        res.status(500).json({ message: "Error al obtener los usuarios"});
+    }
+};
+
+//Obtener por id
+export const getById = async (req, res) => {
+    const id = await req.params.id;
+    try {
+        const getById = await User.findByPk(id);
+        res.status(200).json(getById);
+    } catch (error) {
+        res.status(500).json({ message: "Error al obtener el id"});
+    }
+};
+
+//Crear usuario
+export const createUser = async (req, res) => {
+    const { title, description, isComplete} = await req.body;
+    try {
+        const createUser = await User.create(title, description, isComplete);
+        res.status(201).json(createUser);
+    } catch (error) {
+        res.status(500).json({ message: "Error al crear el usuario"});
+    }
+};
+
+//Actualizar usuario
+export const updateUser = async (req, res) => {
+    const id = req.params.id;
+    const { title, description, isComplete} = await req.body;
+    try {
+        const updateUser = await User.update({ title, description, isComplete }, { where: {id: id}});
+        if(updateUser) {
+            const userUpdated = await User.findByPk(id);
+            res.status(200).json(userUpdated);
+        };
+    } catch (error) {
+        res.status(500).json({ message: "Error al actualizar el usuario"});
+    }
+};
+
+//Eliminar usuario
+export const deleteUser = async (req, res) => {
+    const id = req.params.id;
+    try {
+        const deleteUser = await User.destroy(id);
+        if(deleteUser) {
+            res.status(200).json({ message: "Usuario eliminado con éxito"});
+        };
+    } catch (error) {
+        res.status(500).json({ message: "Error al eliminar el usuario"});
+    }
+};
