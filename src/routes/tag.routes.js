e0b5fcf6a951159e13b00c8187d76d49c06e365a
DylanC <cabreradylan055@gmail.com>
2025-08-25 11:49:37 -0300

    middlewares faltantes agregados a las rutas

--- src/routes/tag.routes.js
+++ src/routes/tag.routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { getAllTags, createTag,GetTagByPk, updateTag, deleteTag } from "../controllers/tag.controller.js";
+import { createTagValidation, deleteTagValidation, getTagByPkValidations, updateTagValidation } from "../middlewares/validations/tag.validations.js";
 
 const router = Router();
 
 router.get('/tags', getAllTags);
-router.get('/tags/:id', GetTagByPk);
-router.post('/tags', createTag);
-router.put('/tags/:id', updateTag);
-router.delete('/tags/:id', deleteTag);
+router.get('/tags/:id', getTagByPkValidations, validator, GetTagByPk);
+router.post('/tags', createTagValidation, validator, createTag);
+router.put('/tags/:id', updateTagValidation, validator, updateTag);
+router.delete('/tags/:id', deleteTagValidation, validator, deleteTag);
 
 export default router;
--- src/routes/taskTag.routes.js
+++ src/routes/taskTag.routes.js
@@ -1,13 +1,15 @@
 import { Router } from "express";
 import { getAllTasksTags, createTasksTags,GetTaskTagByPk, updateTaskTag, deleteTaskTag } from "../controllers/taskTag.controller.js";
+import { createTaskTagValidation, deleteTaskTagValidation, getTaskTagByPkValidations, updateTaskTagValidation } from "../middlewares/validations/taskTag.validations.js";
+
 
 const router = Router();
 
 router.get('/taskstags', getAllTasksTags);
-router.get('/taskstags/:id', GetTaskTagByPk);
-router.post('/taskstags', createTasksTags);
-router.put('/taskstags/:id', updateTaskTag);
-router.delete('/taskstags/:id', deleteTaskTag);
+router.get('/taskstags/:id', getTaskTagByPkValidations, validator, GetTaskTagByPk);
+router.post('/taskstags', createTaskTagValidation, validator, createTasksTags);
+router.put('/taskstags/:id', updateTaskTagValidation, validator, updateTaskTag);
+router.delete('/taskstags/:id',deleteTaskTagValidation , validator ,deleteTaskTag);
 
 export default router;
 
